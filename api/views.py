bbe7c157e2700a9704f35d08cb81da69994edae6
agtz92 <alfredo.gtz92@gmail.com>
2025-06-03 23:14:33 -0600

    webhook

--- api/views.py
+++ api/views.py
@@ -1,4 +1,37 @@
-from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponseBadRequest
+import json
+from .models import ProductoOferta
+from datetime import datetime
+from decimal import Decimal
+from django.conf import settings
 
 def health_check(request):
     return JsonResponse({"status": "ok"})
+
+@csrf_exempt
+def recibir_webhook(request):
+    if request.method != 'POST':
+        return HttpResponseBadRequest("Método no permitido")
+    
+    # Validar token en headers
+    token = request.headers.get("X-Webhook-Token")
+    if token != settings.WEBHOOK_SECRET:
+        return JsonResponse({"error": "Token inválido"}, status=403)
+
+    try:
+        data = json.loads(request.body)
+
+        producto = ProductoOferta.objects.create(
+            titulo=data['titulo'],
+            precio_original=Decimal(str(data['precio_original'])),
+            descuento=Decimal(str(data['descuento'])),
+            precio_oferta=Decimal(str(data['precio_oferta'])),
+            url_imagen=data['url_imagen'],
+            link_referidos=data['link_referidos'],
+            fecha=datetime.strptime(data['fecha'], "%d-%m-%Y").date(),
+            categoria=data['categoria']
+        )
+        return JsonResponse({"status": "ok", "id": producto.id})
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
--- backend/urls.py
+++ backend/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from strawberry.django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from api.schema import schema
-from api.views import health_check
+from api.views import health_check, recibir_webhook
 
 
 urlpatterns = [
     path('', health_check),
     path('admin/', admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(schema=schema))),
+    path('webhook/', recibir_webhook),
 ]
