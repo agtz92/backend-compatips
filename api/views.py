0ae34891b9b21bacc9da580d10daaafe2298946c
agtz92 <alfredo.gtz92@gmail.com>
2025-07-07 22:12:20 -0600

    Update views.py
    last attempt

--- api/views.py
+++ api/views.py
@@ -35,10 +35,10 @@ def recibir_webhook(request):
         try:
             webhook_url = "https://botize.com/webhook/agtz92@2ea6e9221b445044c5c5d91de7227b97ae51e5b2c9bf1fd88056b9aaff8af976/24"
             payload = {
-                "titulo": producto.titulo,
-                "imagen": producto.url_imagen,
-                "precio original": str(producto.precio_original),
-                "precio descuento": str(producto.precio_oferta) if producto.precio_oferta else "",
+                "title": producto.titulo,
+                "img": producto.url_imagen,
+                "originalprice": str(producto.precio_original),
+                "discountprice": str(producto.precio_oferta) if producto.precio_oferta else "",
                 "url": f"https://frontend-compatips-x8tl.vercel.app/producto/{producto.id}"
             }
 
