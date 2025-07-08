c44f280f53b3c65d9d6908a6877b33a58472a700
agtz92 <alfredo.gtz92@gmail.com>
2025-07-07 21:18:32 -0600

    Update schema.py

--- api/schema.py
+++ api/schema.py
@@ -88,10 +88,10 @@ class Mutation:
         try:
             webhook_url = "https://botize.com/webhook/agtz92@2ea6e9221b445044c5c5d91de7227b97ae51e5b2c9bf1fd88056b9aaff8af976/24"
             payload = {
-                "title": producto.titulo,
-                "img": producto.url_imagen,
-                "originalprice": str(producto.precio_original),
-                "discountprice": str(producto.precio_oferta),
+                "titulo": producto.titulo,
+                "imagen": producto.url_imagen,
+                "precio original": str(producto.precio_original),
+                "precio descuento": str(producto.precio_oferta),
                 "url": f"https://frontend-compatips-x8tl.vercel.app/producto/{producto.id}"
             }
             response = requests.post(webhook_url, json=payload)
